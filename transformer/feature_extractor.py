615a05cffc47e4fda3fc06ac8227dbb42296e469
Boris Prokis <bpodolnyi@gmail.com>
2020-10-09 13:27:21 +0300

    Restructured transformer module

--- transformer/feature_extractor.py
+++ transformer/feature_extractor.py
@@ -1,9 +1,4 @@
 from .img_prerprocessor import img_preprocessor
-# from .specular_params import specular_params
-# from .blurriness import *
-# from .chromatic_moments import chromatic_moments
-# from .color_diversity import color_diversity
-
 from .features import *
 
 def extract_features(img):
@@ -15,13 +10,3 @@ def extract_features(img):
     color_features = color_diversity(img_norm)
 
     return np.concatenate((specular_features, blurriness_features, chromatic_features, color_features))
-#
-# def extract_features(img):
-#     img_norm = img_preprocessor(img)
-#
-#     specular_features = specular_params(img_norm)
-#     blurriness_features = np.array([Laplacian_blur_feature(img_norm), Low_pass_blur_feature(img_norm)])
-#     chromatic_features = chromatic_moments(img_norm)
-#     color_features = color_diversity(img_norm)
-#
-#     return np.concatenate((specular_features, blurriness_features, chromatic_features, color_features))
